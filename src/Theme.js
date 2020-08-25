b95d6171feb017fad27cb5eb3688611bda746f04
vanyaxel <vanya_xel@live.com.mx>
2020-08-24 23:19:56 -0500

    register-views

---
+++ src/Theme.js
@@ -0,0 +1,29 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            light: '#8bc4b9',
+            main: '#2F5A62',
+            dark: '#4d7f75',
+            contrastText: '#F7F9FB',
+        },
+        secondary: {
+            light: '#6CA0DE',
+            main: '#435387',
+            dark: '#2e3a5e',
+
+            contrastText: '#F7F9FB',
+        },
+    },
+    typography: {
+        fontFamily: ['Open Sans', 'sans-serif'].join(', '),
+    },
+
+    shape: {
+        borderRadius: 18
+    },
+
+});
+
+export default theme;
